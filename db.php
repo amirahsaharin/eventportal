5ed19f10fef4c1057a2e023351da37c7809065ae
Amirah Saharin <amirahsaharin@gmail.com>
2025-07-02 16:11:35 +0800

    Update db.php to use Azure SQL

--- db.php
+++ db.php
@@ -1,10 +1,11 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = ""; // Default for XAMPP
-$dbname = "event_db";
+$servername = "tcp:yourservername.database.windows.net,1433";  // Azure SQL server
+$username = "sqladmin";        // Your admin username
+$password = "YourStrongPassword!";  // The password you set in Azure
+$dbname = "event_db";          // Your database name in Azure
 
 $conn = new mysqli($servername, $username, $password, $dbname);
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
