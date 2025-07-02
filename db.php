1ecb44f534c2b1a5c7b81cc9aee401b4376ce3da
Amirah Saharin <amirahsaharin@gmail.com>
2025-07-02 19:14:30 +0800

    Azure MySQL connection

--- db.php
+++ db.php
@@ -4,11 +4,12 @@ $username = "mysqladmin@eventmysql123";  // Your full user name
 $password = "MyEventDB123!";     // Your actual password
 $dbname = "event_db";
 
-// ✅ Enable SSL
 $conn = mysqli_init();
 mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
 
 if (!$conn->real_connect($servername, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL)) {
-    die("Connection failed: " . mysqli_connect_error());
+    die("❌ Connection failed: " . mysqli_connect_error());
+} else {
+    echo "✅ Connected successfully to Azure MySQL!";
 }
 ?>
